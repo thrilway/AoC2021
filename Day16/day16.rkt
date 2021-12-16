9338ad8b476f30ef4b8f78c0f3966fe10437e40b
Dan Milway <dan.milway@gmail.com>
2021-12-16 13:30:04 -0500

    modified:   day16.rkt

--- Day16/day16.rkt
+++ Day16/day16.rkt
@@ -20,23 +20,14 @@
     (let ((id (string->number (substring bstr 0 3) 2)) (r (substring bstr 3)))
       (if (equal? 4 id)
           (values r 'literal)
-          (let ((op
-                 (case id
-                    ((0) 'plus)
-                    ((1) 'minus)
-                    ((2) 'mini)
-                    ((3) 'maxi)
-                    ((5) 'gt)
-                    ((6) 'lt)
-                    ((7) 'equ))))
-            (if (char=? #\0 (string-ref r 0))
+          (if (char=? #\0 (string-ref r 0))
                 (let* ((len (string->number (substring r 1 16) 2))
                           (b (substring r 16 (+ 16 len)))
                           (new-r (substring r (+ 16 len))))
-                  (values b (cons op new-r)))
+                  (values b (cons id new-r)))
                 (let ((num (string->number (substring r 1 12) 2))
                       (b (substring r 12)))
-                  (values b (cons op num)))))))))
+                  (values b (cons id num))))))))
 (define (literal bstr)
   (lambda ()
     (let loop ((rem bstr) (acc ""))
@@ -45,51 +36,51 @@
            (string->number (string-append acc (substring rem 1 5)) 2)
            (substring rem 5))
           (loop (substring rem 5) (string-append acc (substring rem 1 5)))))))
-(define (call op x)
-  (cond
-    ((eqv? op 'plus) (apply + x))
-    ((eqv? op 'minus) (apply * x))
-    ((eqv? op 'mini) (apply min x))
-    ((eqv? op 'maxi) (apply max x))
-    ((eqv? op 'gt) (if (apply > x) 1 0))
-    ((eqv? op 'lt) (if (apply < x) 1 0))
-    ((eqv? op 'equ) (if (apply equal? x) 1 0))))
 
 (define (read-packet bin (op '()) (num 0))
+  (define op-table
+    (hash
+     0 +
+     1 *
+     2 min
+     3 max
+     5 (lambda (x y) (if (> x y) 1 0)) 
+     6 (lambda (x y) (if (< x y) 1 0))
+     7 (lambda (x y) (if (equal? x y) 1 0))))
   (let loop ((rem bin) (stack '()) (operands '()) (sum 0))
     (cond ((and
-            (not (zero? num))
-            (equal? num (length operands)))
-           (values rem (call op operands) sum))
-          ((and (not (null? op))
-                (zero? num)
-                (null? stack)
-                (string=? rem (make-string (string-length rem) #\0)))
-           (values (call op operands) sum))
-          ((and (null? op)
-                (null? stack)
-                (string=? rem (make-string (string-length rem) #\0)))
-           (car operands))
-          ((null? stack)
+            (not (zero? num)) ; Operating on a fixed number of values
+            (equal? num (length operands))) ; You have enough values
+           (values rem (apply (hash-ref op-table op) operands) sum)) ; run the operation
+          ((and (not (null? op)) ; You've got an operator
+                (zero? num)      ; operation on the wohle input
+                (null? stack)    
+                (string=? rem (make-string (string-length rem) #\0))) ; no more packets to read
+           (values (apply (hash-ref op-table op) operands) sum)) ; Evaluate it
+          ((and (null? op) ; no operation
+                (null? stack) 
+                (string=? rem (make-string (string-length rem) #\0))) ;nothing left to read
+           (car operands)) ; DONE!
+          ((null? stack) ; Start
            (call-with-values
             (read-vers rem)
             (lambda (r code v)
               (loop r (cons code stack) operands (+ sum v)))))
-          ((equal? 'id (car stack))
+          ((equal? 'id (car stack)) ; What kind of packet?
            (call-with-values
             (read-id rem)
             (lambda (r code)
               (loop r (cons code (cdr stack)) operands sum))))
-          ((equal? 'literal (car stack))
+          ((equal? 'literal (car stack)) ; read a literal and add it to the operands 
            (call-with-values (literal rem)
                              (lambda (lit r)
                                (loop r (cdr stack) (append operands (list lit)) sum))))
-          ((string? (cdar stack))
+          ((string? (cdar stack)) ;operate on a fixed string
            (call-with-values
             (lambda () (read-packet rem (caar stack)))
             (lambda (val s)
               (loop (cdar stack) (cdr stack) (append operands (list val)) (+ sum s)))))
-          ((number? (cdar stack))
+          ((number? (cdar stack)) ;operate on a number of packets
            (call-with-values
             (lambda () (read-packet rem (caar stack) (cdar stack)))
             (lambda (r val s)
